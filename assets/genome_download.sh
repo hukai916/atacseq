7ea27cd77cf8350cc473cefa9089df88006ebd4f
hukai916 <hukai916@gmail.com>
2026-03-31 13:43:02 -0400

    custom config

---
+++ assets/genome_download.sh
@@ -0,0 +1,7 @@
+wget https://ftp.ensembl.org/pub/release-110/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
+
+wget https://ftp.ensembl.org/pub/release-110/gtf/mus_musculus/Mus_musculus.GRCm39.110.gtf.gz
+
+Blacklist: 
+https://bedbase.org/bed/67c08efd44d558f0aafbb1240e93a97a
+https://github.com/Boyle-Lab/Blacklist/issues/25
